e5470d04d602365f520a33e0da32eb574449c4d5
Benjamin Navarro <navarro.benjamin13@gmail.com>
2026-09-17 15:02:00 +0200

    build: cleaner build and proper target installation

--- cmake/install_targets.cmake
+++ cmake/install_targets.cmake
@@ -1,14 +1,24 @@
 include(CMakePackageConfigHelpers)
+
+install(
+  EXPORT btcppTargets
+  FILE btcppTargets.cmake
+  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/btcpp
+  NAMESPACE btcpp::
+)
+
 configure_package_config_file(
     "${CMAKE_CURRENT_SOURCE_DIR}/cmake/btcppConfig.cmake.in"
     "${CMAKE_CURRENT_BINARY_DIR}/btcppConfig.cmake"
     INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/btcpp"
 )
+
 write_basic_package_version_file(
     ${CMAKE_CURRENT_BINARY_DIR}/btcppConfigVersion.cmake
     VERSION ${CMAKE_PROJECT_VERSION}
     COMPATIBILITY SameMajorVersion
 )
+
 install(
     FILES
         "${CMAKE_CURRENT_BINARY_DIR}/btcppConfig.cmake"
